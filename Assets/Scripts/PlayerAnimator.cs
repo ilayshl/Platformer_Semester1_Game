58b3c6f4e436611e497a11e1b68c5429268215d7
Ilay Sh <ilayshlomo@gmail.com>
2024-12-17 14:47:51 +0200

    16.12 pc
    changed a lot of stuff idk
    transitioned to sidescroll instead of top down

--- Assets/Scripts/PlayerAnimator.cs
+++ Assets/Scripts/PlayerAnimator.cs
@@ -3,41 +3,41 @@ using System.Collections.Generic;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
-public class Playeranim : MonoBehaviour
-{
+public class Playeranim: MonoBehaviour {
     private Animator anim;
     private PlayerMovement pm;
     private SpriteRenderer sr;
+    private Rigidbody2D rb;
+    private Vector2 playerSpeed;
 
-    void Start()
-    {
-        anim = GetComponent<Animator>();
-        pm = GetComponent<PlayerMovement>();
-        sr = GetComponent<SpriteRenderer>();
+    void Start() {
+        anim=GetComponent<Animator>();
+        pm=GetComponent<PlayerMovement>();
+        sr=GetComponent<SpriteRenderer>();
+        rb=GetComponent<Rigidbody2D>();
     }
 
-    void Update()
-    {
-        if (pm.moveDir.x != 0 || pm.moveDir.y != 0)
-        {
+    void Update() {
+        if(pm.horizontalDir!=0) {
             anim.SetBool("isMoving", true);
             DirectionChanger();
-        }
-        else
-        {
+        } else {
             anim.SetBool("isMoving", false);
         }
+        if(pm.verticalDir<0) {
+            anim.SetBool("isCrouching", true);
+        } else {
+            anim.SetBool("isCrouching", false);
+        }
+        anim.SetFloat("yVelocity", rb.velocity.y);
+
     }
 
-    void DirectionChanger()
-    {
-        if (pm.moveDir.x < 0)
-        {
-            sr.flipX = true;
-        }
-        else if (pm.moveDir.x > 0)
-        {
-            sr.flipX = false;
+    void DirectionChanger() {
+        if(pm.horizontalDir<0) {
+            sr.flipX=true;
+        } else if(pm.horizontalDir>0) {
+            sr.flipX=false;
         }
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,37 +2,58 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
-{
+public class PlayerMovement: MonoBehaviour {
 
     public float moveSpeed;
-    public Vector2 moveDir;
+    public float horizontalDir;
+    public float verticalDir;
+    public bool isJumping = false;
+    [SerializeField] int jumpForce = 7;
+    [SerializeField] float crouchSlowMultiplier = 1.5f;
+    private float moveSpeedMultiplier; //For running animation- how fast will it reach maximum speed
     Rigidbody2D rb;
 
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
+    void Start() {
+        rb=GetComponent<Rigidbody2D>();
     }
 
-    void Update()
-    {
+    private void Update() {
         InputManagement();
+        CheckForJump();
     }
 
-    void FixedUpdate()
-    {
+    void FixedUpdate() {
         Move();
     }
 
-    void InputManagement()
-    {
-        float moveHorizontal = Input.GetAxisRaw("Horizontal");
-        float moveVertical = Input.GetAxisRaw("Vertical");
-        moveDir = new Vector2(moveHorizontal, moveVertical).normalized;
+    void InputManagement() {
+        horizontalDir=Input.GetAxisRaw("Horizontal");
+        verticalDir=Input.GetAxisRaw("Vertical");
     }
 
-    void Move()
-    {
-        rb.velocity = new Vector2(moveDir.x * moveSpeed, moveDir.y * moveSpeed);
+    void CheckForJump() {
+        if(isJumping==false&&verticalDir>0) {
+            rb.AddForce(new Vector2(rb.velocity.x, jumpForce), ForceMode2D.Impulse);
+            isJumping=true;
+            GetComponent<Animator>().SetBool("isAirborne", true);
+        }
+    }
+
+    void Move() {
+        if(verticalDir<0 && isJumping==false) {
+            rb.velocity=new Vector2(horizontalDir*moveSpeed/crouchSlowMultiplier, rb.velocity.y);
+        } else {
+            rb.velocity=new Vector2(horizontalDir*moveSpeed, rb.velocity.y);
+        }
+        
+    }
+
+
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if(isJumping==true) {
+            isJumping=false;
+            GetComponent<Animator>().SetBool("isAirborne", false);
+        }
     }
 }
